254812545e282120f4fed6bbc6b1be7006b51d45
bhaskar945521 <mallbhaskar62004@gmail.com>
2025-07-27 13:03:05 +0530

    .

--- Routes/AuthRoutes.js
+++ Routes/AuthRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 // ✅ Import controller functions
-const { register, login } = require('../Controllers/AuthController.js');
+const { register, login } = require('../Controllers/AuthController');
 
 // ✅ Import middlewares
-const verifyToken = require('../Middleware/verifyToken');
+const verifyToken = require('../Middleware/verifyToken.js');
 const IsAdmin = require('../Middleware/IsAdmin');
 
 // ✅ Public Login Route
